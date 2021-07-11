cb6766853e79191bd11e4d33d2ef06c42b7a5ae0
jerome-dubois <jerome.dubois@gmail.com>
2021-07-11 13:29:06 +0200

    update multer

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,9 +1,12 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
     const thing = new Thing({
-        ...req.body
+        ...thingObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     thing.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
@@ -11,21 +14,33 @@ exports.createThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({message : 'Objet modifié !'}))
+    const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id })
+        .then(() => res.status(200).json({message : 'Object modified !'}))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
+        .then((thing) => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({message : 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => {
+            const filename = thing.imageUrl.split('/images')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({message : 'Object deleted !'}))
+                    .catch(error => res.status(400).json({ error }));
+            })
+        })
+        .catch(error => res.status(500).json({ error })); 
 };
 
 exports.getAllStuff = (req, res, next) => {
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff');
 
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing); 
 router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);  
+router.get('/:id', auth, stuffCtrl.getOneThing); 
 router.delete('/:id', auth, stuffCtrl.deleteThing);
-
 
 module.exports = router;
